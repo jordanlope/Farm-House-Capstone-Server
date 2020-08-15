04afb86c06c613aecee3b7993e6aff559bfc5a96
Jordan Lopez <jordanlopez992@gmail.com>
2020-08-15 00:46:43 -0400

    Set up DB and Tables within

---
+++ postgrator-config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": 'postgresql://jordanlopez992@localhost/farmhouse',
+}
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_URL: "postgresql://jordanlopez992@localhost/farmhouse",
+    DB_URL_TEST: process.env.DB_URL_TEST
 }
