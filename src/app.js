72ce620b8573271dd0dc52e547fec0feeb233c23
Jordan Lopez <jordanlopez992@gmail.com>
2020-08-22 19:49:01 -0400

    Ready for deploy

--- src/app.js
+++ src/app.js
@@ -39,12 +39,12 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
+    // if (NODE_ENV === 'production') {
+    //     response = { error: { message: 'server error' } }
+    // } else {
         console.error(error)
         response = { message: error.message, error }
-    }
+    // }
     res.status(500).json(response)
 })
 
--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL,
-    DB_URL_TEST: process.env.DB_URL_TEST,
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://jordanlopez992@localhost/farmhouse",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://jordanlopez992@localhost/farmhouse_test",
     JWT_SECRET: '4501118c-e1c1-11ea-87d0-0242ac130003',
     JWT_EXPIRY: '20s'
 }
