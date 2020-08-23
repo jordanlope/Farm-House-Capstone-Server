32afde6e832dbcd738005ac63925e6a3f5fb90eb
Jordan Lopez <jordanlopez992@gmail.com>
2020-08-22 20:00:47 -0400

    Fix before deploy

--- src/app.js
+++ src/app.js
@@ -19,32 +19,32 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use('/api/farmHouses', farmHousesRouter)
-app.use('/api/realtors', realtorsRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/users', usersRouter)
-
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
 
     const authToken = req.get('Authorization')
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        logger.error(`Unauthorized request to path: ${req.path}`);
+        //logger.error(`Unauthorized request to path: ${req.path}`);
         return res.status(401).json({ error: 'Unauthorized request' })
     }
     // move to the next middleware
     next()
 })
 
+app.use('/api/farmHouses', farmHousesRouter)
+app.use('/api/realtors', realtorsRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/users', usersRouter)
+
 app.use(function errorHandler(error, req, res, next) {
     let response
-    // if (NODE_ENV === 'production') {
-    //     response = { error: { message: 'server error' } }
-    // } else {
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
         console.error(error)
         response = { message: error.message, error }
-    // }
+    }
     res.status(500).json(response)
 })
 
